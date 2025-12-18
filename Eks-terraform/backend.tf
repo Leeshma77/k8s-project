a8532bbbe59e9e8a95fb7802d4eaf952cf737abe
Leeshma77 <leeshma2025@gmail.com>
2025-12-18 16:43:01 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform.eks.leeshma" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
 use_lockfile =true
   }
 }
