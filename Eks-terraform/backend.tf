1f6628bb4e81b3ad9de9a2aa7736c5e21c5eb0fc
Leeshma77 <leeshma2025@gmail.com>
2025-12-19 15:15:06 +0530

    Change S3 bucket name in backend configuration

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform.eks.leeshma" # Replace with your actual S3 bucket name
+    bucket = "mybucketleeshma" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
 use_lockfile =true
